9fc95f8626fa64635359a2cc0bc9730455d15520
ashok4432 <42637606+ashok4432@users.noreply.github.com>
2025-07-03 07:23:35 -0500

    Update SpringbootAppApplication.java

--- src/main/java/com/example/SpringbootAppApplication.java
+++ src/main/java/com/example/SpringbootAppApplication.java
@@ -1,12 +1,12 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class SpringbootAppApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootAppApplication.class, args);
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot on EKS!";
     }
 }
